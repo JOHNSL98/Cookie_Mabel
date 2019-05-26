fe1983e9641216b60f6ecefdcd6eb672023d1735
JOHNSL <johnasuarezl@gmail.com>
2019-05-26 10:18:58 -0500

    Primer commit Mabel

---
+++ Assets/Scripts/Click.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Click : MonoBehaviour {
+    
+    public UnityEngine.UI.Text gpc;
+    public UnityEngine.UI.Text goldDisplay;
+    public float gold = 0.00f;
+    public int goldperclick = 1;
+    
+    void Update() {
+		goldDisplay.text = "Gold: " + CurrencyConverter.Instance.GetCurrencyIntoString(gold, false, false);
+		gpc.text = CurrencyConverter.Instance.GetCurrencyIntoString(goldperclick, false, true) + " gold/click";
+    }
+    
+    public void Clicked() {
+		gold += goldperclick;
+    }
+}
---
+++ Assets/Scripts/Scene_Change.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene_Change : MonoBehaviour {
+
+	public void SceneChange(int SceneNumber) {
+		SceneManager.LoadScene (SceneNumber);
+	}
+}
